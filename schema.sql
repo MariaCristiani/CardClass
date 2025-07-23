652638e686c29dfedf0bc3931d4bdf6fa907aa23
Amanda <161896943+AmandaA6@users.noreply.github.com>
2025-07-23 20:22:51 -0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -3,4 +3,13 @@ CREATE TABLE IF NOT EXISTS users(
     nome TEXT NOT NULL,
     email TEXT NOT NULL UNIQUE,
     senha TEXT NOT NULL
-)
+);
+
+CREATE TABLE flashcards (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    pergunta TEXT NOT NULL,
+    resposta TEXT NOT NULL,
+    materia TEXT NOT NULL,
+    id_usuario INTEGER,
+    FOREIGN KEY (id_usuario) REFERENCES usuarios(id)
+);
